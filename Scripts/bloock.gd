aa6acaf4671ea5563cf8e470f7c78f1ad5dcf06e
Brady Raye Kearley <bradykearley84@gmail.com>
2026-02-10 12:49:09 -0500

    Changed moving shapes to be one click to pickup one to drop.

--- Scripts/bloock.gd
+++ Scripts/bloock.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
-var mouseHover:bool = false
+var mouseHover: bool = false
+var isPickedUp: bool = false
 @export var offset: Vector2
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed and mouseHover:
+		isPickedUp = !isPickedUp
 
 func _process(delta: float) -> void:
 	#Deletes blocks off screen
@@ -9,12 +14,13 @@ func _process(delta: float) -> void:
 		queue_free()
 	
 	#This code basicaly makes snapping work on all blocks was a huge pain with rotations and everything but it works
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and mouseHover:
+	if isPickedUp:
 		var rotated_offset = offset.rotated(rotation)
-		var addition_offset = Vector2(0,0)
+		var addition_offset = Vector2(0, 0)
 		if int(rotation_degrees) % 180 == 0:
-			addition_offset = Vector2(12,12)
+			addition_offset = Vector2(12, 12)
 		global_position = get_global_mouse_position().snapped(Vector2(24, 24)) + rotated_offset + addition_offset
+	
 	if mouseHover and Input.is_action_just_pressed("Rotate"):
 		rotation += deg_to_rad(90)
 
@@ -24,6 +30,4 @@ func _on_area_2d_mouse_entered() -> void:
 
 
 func _on_area_2d_mouse_exited() -> void:
-	#coyote frames sorta deal.
-	await get_tree().create_timer(.2).timeout
 	mouseHover = false
