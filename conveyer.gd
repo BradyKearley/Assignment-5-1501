2687f63ff959f055c620b6d6a33ee0b6b9dcaec3
Brady Raye Kearley <bradykearley84@gmail.com>
2026-02-10 12:41:41 -0500

    Conveyer script

--- conveyer.gd
+++ conveyer.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var direction: Vector2 = Vector2.RIGHT 
+@export var direction: Vector2 = Vector2.RIGHT
 @export var speed: float = 100.0
 
 func _physics_process(delta: float) -> void:
