1b960f7a652762d564ab8b9db7ff730218150c18
Kuppablo <saintpaaablo@gmail.com>
2026-02-22 15:15:23 -0500

    AnimSprite2D implementation

--- node_2d.gd
+++ node_2d.gd
@@ -1,25 +1,17 @@
 extends Node2D
 
-@onready var frame1 = $Sprite2D
-@onready var frame2 = $Sprite2D2
 @onready var sound1 = $AudioStreamPlayer2D
 @onready var sound2 = $AudioStreamPlayer2D2
 @onready var sound3 = $AudioStreamPlayer2D3
+@onready var anim = $AnimatedSprite2D
 var random = RandomNumberGenerator.new()
 var played = false
 var timer = 0.0
-var timer2 = 0.0
 
 func _process(delta: float) -> void:
 	timer += 1 * delta
 	if timer >= 30:
-		timer2 += 5 * delta
-		if int(timer2)%2 == 0:
-			frame1.visible = true
-			frame2.visible = false;
-		else:
-			frame1.visible = false
-			frame2.visible = true
+		anim.play()
 		if !played:
 			var randomint = random.randi_range(1,3)
 			if randomint == 1:
@@ -30,9 +22,8 @@ func _process(delta: float) -> void:
 				sound3.play()
 			played = true
 	else:
-		timer2 = 0
-		frame1.visible = true
-		frame2.visible = false;
+		anim.pause()
+		anim.frame = 0
 
 func _on_audio_stream_player_2d_finished() -> void:
 	timer = 0
