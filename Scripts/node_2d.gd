40ba29251fb88107de90ce1d51794d1337ecfe38
Cristiano Sellitto <cristiano.sellitto@gmail.com>
2026-02-22 18:04:43 -0500

    Stop game when finishing it

--- Scripts/node_2d.gd
+++ Scripts/node_2d.gd
@@ -6,6 +6,7 @@ var current_shape: Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Create the initial shape to create
+	$Shapes.text = "Shapes Completed: " + str(shapes_done) + "/10"
 	create_shape(shapes_done + 1)
 
 
@@ -18,16 +19,22 @@ func _on_shape_finished() -> void:
 	# Create the next shape to do
 	shapes_done += 1
 	call_deferred("remove_child", current_shape)
+	# Destroy all blocks in the scene
+	for block in get_tree().get_nodes_in_group("Block"):
+		block.queue_free()
 	if shapes_done != 10:
-		# If the player is still making shapes
+		# If the player is still making shapes then create the next one
+		$Shapes.text = "Shapes Completed: " + str(shapes_done) + "/10"
 		create_shape(shapes_done + 1)
-		# Destroy all blocks in the scene
-		for block in get_tree().get_nodes_in_group("Block"):
-			block.queue_free()
-		# Play victory sound
-		$Victory.play()
 	else:
-		print("Complete")
+		# If the player is done then stop the game
+		$Shapes.text = "You did it!"
+		$BoxSpawner.process_mode = Node.PROCESS_MODE_DISABLED
+		$BoxSpawner2.process_mode = Node.PROCESS_MODE_DISABLED
+		$Node2D.process_mode = Node.PROCESS_MODE_DISABLED
+		$Timer/Timer.process_mode = Node.PROCESS_MODE_DISABLED
+	# Play victory sound
+	$Victory.play()
 
 
 func create_shape(number: int) -> void:
